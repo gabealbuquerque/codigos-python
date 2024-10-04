5ad79a0af943c4ad9f535a18d390272ab55f3082
Gabriel de Albuquerque Vasconcelos <eugabrieldealbuquerque@gmail.com>
2024-10-03 22:54:50 -0300

    Create diferenca.py

---
+++ codigos-python/diferenca.py
@@ -0,0 +1,6 @@
+A = int(input('Digite um valor para A: '))
+B = int(input('Digite um valor para B: '))
+C = int(input('Digite um valor para C: '))
+D = int(input('Digite um valor para D: '))
+diferenca = (A * B - C * D)
+print(f'A diferença é de: {diferenca}.')
