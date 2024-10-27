5a31afbb715f3f438c7d517d544c0556076e1b25
Gabriel de Albuquerque Vasconcelos <eugabrieldealbuquerque@gmail.com>
2024-10-27 16:31:52 -0300

    att

--- ex107/teste.py
+++ ex107/teste.py
@@ -2,6 +2,7 @@ from ex107 import moedas
 from ex107.moedas import moeda
 
 p = float(input('Digite o preço: R$ '))
-print(f'A metade de {moedas.moeda(p)} é {moedas.moeda(moedas.metade(p))}')
-print(f'O dobro de {moedas.moeda(p)} é {moedas.moeda(moedas.dobro(p))}')
-print(f'Aumentando 10%, temos R$ {moedas.moeda(moedas.aumentar(p, 10))}')
+print(f'A metade de {moedas.moeda(p)} é {moedas.metade(p, True)}')
+print(f'O dobro de {moedas.moeda(p)} é {moedas.dobro(p, True)}')
+print(f'Aumentando 10%, temos {moedas.aumentar(p, 10, True)}')
+print(f'Reduzindo 13%, temos {moedas.diminuir(p, 13, True)}')
