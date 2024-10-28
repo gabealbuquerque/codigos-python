ccee39714c6a88cb57fb0c77eecba8b82264c88d
Gabriel de Albuquerque Vasconcelos <eugabrieldealbuquerque@gmail.com>
2024-10-28 13:53:04 -0300

    att

---
+++ ex115/sistema.py
@@ -0,0 +1,26 @@
+from ex115.lib.interface import *
+from ex115.lib.arquivo import *
+from time import sleep
+
+arq = 'cursoemvideo.txt'
+
+if not arquivoExiste(arq):
+    criarArquivo(arq)
+
+while True:
+    resposta = menu(['Ver pessoas cadastradas', 'Cadastrar nova Pessoa', 'Sair do Sistema'])
+    if resposta == 1:
+        # Opção de listar o conteúdo de um aquivo.
+        lerArquivo(arq)
+    elif resposta == 2:
+        # Opção de cadastrar uma nova pessoa.
+        cabecalho('NOVO CADASTRO')
+        nome = input('Nome: ')
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
+    elif resposta == 3:
+        cabecalho('Saindo do sistema... Até logo!')
+        break
+    else:
+        print('\033[0;31mErro! Digite uma opção válida!\033[m')
+    sleep(1)
